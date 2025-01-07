48d757531267ebdb06999c7c884a62a182194515
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-07 21:19:13 +0100

    feat: auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./modules/users/user.routes";
+import {auth} from "./middleware/auth";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(auth);
 
 app.use('/users', userRoutes);
 
