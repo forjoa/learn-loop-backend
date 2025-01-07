28164a0710a641d4a01c0d269820d7d8bb5c0665
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-07 16:57:12 +0100

    feat: user route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import cors from "cors";
+import userRoutes from "./modules/user.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/users', userRoutes);
 
 export default app;
