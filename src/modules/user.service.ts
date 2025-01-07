081c0779463dc132529c932ef3b4223198a6bec7
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-07 16:46:53 +0100

    build: service logic

--- src/modules/user.service.ts
+++ src/modules/user.service.ts
@@ -1,3 +1,14 @@
 import prisma from "../config/db";
 import {CreateUserInput} from "./user.model";
 import bcrypt from "bcryptjs";
+
+export const createUser = async (user: CreateUserInput) => {
+    const hashedPassword = await bcrypt.hash(user.password, 10)
+
+    return prisma.user.create({
+        data: {
+            ...user,
+            password: hashedPassword
+        }
+    })
+}
