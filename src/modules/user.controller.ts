3d9d76c15997c83a834696bc52a0041958d9f9e0
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-07 16:52:54 +0100

    build: handle request

---
+++ src/modules/user.controller.ts
@@ -0,0 +1,27 @@
+import {Request, Response} from "express";
+import {createUserSchema} from "./user.model";
+import {createUser} from "./user.service";
+
+export const handleCreateUser = async (req: Request, res: Response) => {
+    try {
+        // validate request body using zod
+        const validateData = createUserSchema.parse(req.body);
+
+        // call service to create user
+        const user = await createUser(validateData);
+
+        return res.status(201).json({
+            message: 'User created successfully',
+            data: user
+        })
+    } catch (error) {
+        if (error instanceof Error && 'issues' in error) {
+            return res.status(400).json({
+                message: 'Validation error',
+                details: error.issues
+            })
+        }
+
+        return res.status(500).json({message: 'Internal server error'})
+    }
+}
