38b57d4eecee8469ccf6108b358fe04e707619a1
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 16:05:13 +0100

    build: add signature to env varibles

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,5 +4,6 @@ config();
 
 export const env = {
     DATABASE_URL: process.env.DATABASE_URL,
-    API_KEY: process.env.API_KEY
+    API_KEY: process.env.API_KEY,
+    SIGNATURE: process.env.SIGNATURE
 }
