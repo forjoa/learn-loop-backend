2484e4a648f975380866902a2ac73c4d7f7088ef
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 16:23:09 +0100

    refactor: update auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,25 @@
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 import {env} from "../config/env";
+import jwt from "jsonwebtoken";
 
-export const auth = (req: Request, res: Response, next: Function) => {
-    if (req.headers['x-api-key'] !== env.API_KEY) {
-        return res.status(401).json({message: 'Unauthorized'});
+interface AuthenticateRequest extends Request {
+    user?: { userId: number, role: string }
+}
+
+export const auth = (req: AuthenticateRequest, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) return res.status(401).json({message: 'Unauthorized'});
+
+    try {
+        const secret = env.SIGNATURE;
+        const payload = jwt.verify(token, secret) as { userId: number, role: string };
+
+        // save user info in request
+        req.user = payload;
+
+        next();
+    } catch (e) {
+        return res.status(401).json({message: 'Unauthorized'})
     }
-    next();
 }
