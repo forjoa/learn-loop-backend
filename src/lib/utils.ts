87bde8f17d7f15bd60e4fe9df3a90332d93da578
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 16:06:25 +0100

    refactor: get signature from env

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import {env} from "../config/env";
 
 export const generateToken = (userId: number, role: string) => {
-    const secret = "super_secret";
+    const secret = env.SIGNATURE;
     const expiresIn = "1h";
 
     return jwt.sign({userId, role}, secret, {expiresIn});
