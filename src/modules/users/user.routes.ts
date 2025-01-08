9617c7d82a292dd3eea5ac1ebfbd12e690b164dc
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 19:10:54 +0100

    feat: implement endpoint to edit user

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,5 +1,9 @@
 import {Router} from "express";
+import {handleEditUser} from "./user.controller";
 
 const router = Router();
+
+// @ts-ignore
+router.post('/edit', handleEditUser)
 
 export default router;
