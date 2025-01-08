c39434b00b59567bcd94a5634be8504df7db2564
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 14:15:41 +0100

    build: logic service for login

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,6 +1,7 @@
 import prisma from "../../config/db";
-import {CreateUserInput} from "./user.model";
+import {CreateUserInput, LoginUserInput} from "./user.model";
 import bcrypt from "bcryptjs";
+import {generateToken} from "../../lib/utils";
 
 export const createUser = async (user: CreateUserInput) => {
     const hashedPassword = await bcrypt.hash(user.password, 10)
@@ -11,4 +12,28 @@ export const createUser = async (user: CreateUserInput) => {
             password: hashedPassword
         }
     })
+}
+
+export const loginUser = async (input: LoginUserInput) => {
+    // find user by email
+    const user = await prisma.user.findUnique({
+        where: {
+            email: input.email
+        }
+    })
+
+    if (!user) {
+        throw new Error('User not found')
+    }
+
+    // verify password
+    const isPasswordValid = await bcrypt.compare(input.password, user.password)
+
+    if (!isPasswordValid) {
+        throw new Error('Invalid password')
+    }
+
+    const token = generateToken(user.id, user.role)
+
+    return {token, user: {id: user.id, email: user.email, role: user.role}}
 }
