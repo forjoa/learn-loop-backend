f5afee0af1cad70233a0136f0026414162b61357
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 14:09:07 +0100

    build: schema for login

--- src/modules/users/user.model.ts
+++ src/modules/users/user.model.ts
@@ -8,3 +8,10 @@ export const createUserSchema = z.object({
 })
 
 export type CreateUserInput = z.infer<typeof createUserSchema>
+
+export const loginUserSchema = z.object({
+    email: z.string().email("Invalid email address").min(1, "Email is required"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+})
+
+export type LoginUserInput = z.infer<typeof loginUserSchema>
