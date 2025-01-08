2d4bd283fc28d4ae10978104c48c57b9d8859ca1
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 19:08:00 +0100

    build: logic service to update user

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,5 +1,6 @@
 import {EditUserInput} from "./user.model";
+import prisma from "../../config/db";
 
 export const editUser = async (user: EditUserInput) => {
-    return prisma.user.update({where: {id: user}, data: user})
+    return prisma.user.update({where: {id: user.id}, data: user})
 }
