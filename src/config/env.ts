37d0eb196f8b7418eb5949cc578668a50e4c3f43
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-08 16:07:39 +0100

    refactor: test before return

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,5 +5,5 @@ config();
 export const env = {
     DATABASE_URL: process.env.DATABASE_URL,
     API_KEY: process.env.API_KEY,
-    SIGNATURE: process.env.SIGNATURE
+    SIGNATURE: process.env.SIGNATURE || 'abcd'
 }
