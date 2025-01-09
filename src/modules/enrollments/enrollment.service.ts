1a44d0fb81afe81695f4bc4448ee48e4d26dcc77
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-09 13:01:38 +0100

    build: accept enrollment service

--- src/modules/enrollments/enrollment.service.ts
+++ src/modules/enrollments/enrollment.service.ts
@@ -1,6 +1,10 @@
-import {CreateEnrollmentSchema} from "./enrollment.model";
+import {AcceptEnrollmentSchema, CreateEnrollmentSchema} from "./enrollment.model";
 import prisma from "../../config/db";
 
 export const createEnrollment = async (enrollment: CreateEnrollmentSchema) => {
     return prisma.enrollment.create({data: enrollment})
+}
+
+export const acceptEnrollment = async (enrollment: AcceptEnrollmentSchema) => {
+    return prisma.enrollment.update({where: {id: enrollment.id}, data: enrollment})
 }
