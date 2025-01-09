d639efbe1966ff41cc67a13c2904288f23fb8d4c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-09 12:57:20 +0100

    build: accept enrollment model

--- src/modules/enrollments/enrollment.model.ts
+++ src/modules/enrollments/enrollment.model.ts
@@ -7,3 +7,11 @@ export const createEnrollmentSchema = z.object({
 })
 
 export type CreateEnrollmentSchema = z.infer<typeof createEnrollmentSchema>
+
+export const acceptEnrollmentSchema = z.object({
+    userId: z.number(),
+    topicId: z.number(),
+    status: z.enum(["PENDING", "APPROVED", "REJECTED"]).default("APPROVED")
+})
+
+export type AcceptEnrollmentSchema = z.infer<typeof acceptEnrollmentSchema>
