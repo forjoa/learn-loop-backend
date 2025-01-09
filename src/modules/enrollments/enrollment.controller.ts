57faa3c491661fa3563ae1eef2af167435f8639d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-09 13:14:08 +0100

    build: accept enrollment controller

--- src/modules/enrollments/enrollment.controller.ts
+++ src/modules/enrollments/enrollment.controller.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
-import {createEnrollmentSchema} from "./enrollment.model";
-import {createEnrollment} from "./enrollment.service";
+import {acceptEnrollmentSchema, createEnrollmentSchema} from "./enrollment.model";
+import {acceptEnrollment, createEnrollment} from "./enrollment.service";
 
 export const handleCreateEnrollment = async (req: Request, res: Response) => {
     try {
@@ -12,6 +12,30 @@ export const handleCreateEnrollment = async (req: Request, res: Response) => {
 
         return res.status(200).json({
             message: 'Enrollment created successfully',
+            data: enrollment
+        })
+    } catch (error) {
+        if (error instanceof Error && 'issues' in error) {
+            return res.status(400).json({
+                message: 'Validation error',
+                details: error.issues
+            })
+        }
+
+        return res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+export const handleAcceptEnrollment = async (req: Request, res: Response) => {
+    try {
+        // validate request body using zod
+        const validateData = acceptEnrollmentSchema.parse(req.body)
+
+        // call service to accept
+        const enrollment = await acceptEnrollment(validateData)
+
+        return res.status(200).json({
+            message: 'Enrollment accepted successfully',
             data: enrollment
         })
     } catch (error) {
