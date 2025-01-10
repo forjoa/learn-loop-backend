1a1c63c7dc30b65e1ae816007754b7961de6c82e
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-10 14:15:02 +0100

    refactor: update create message model

--- src/modules/messages/message.model.ts
+++ src/modules/messages/message.model.ts
@@ -3,7 +3,6 @@ import {z} from "zod";
 export const createMessageSchema = z.object({
     content: z.string().min(1, "Message shouldn't be empty"),
     senderId: z.number(),
-    receiverId: z.number(),
     chatId: z.number()
 })
 
