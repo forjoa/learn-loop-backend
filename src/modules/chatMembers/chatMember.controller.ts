2cab58cb02dcb3339fe8b95db0aa8050ffe5c437
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-10 22:38:56 +0100

    build: delete chat member controller

--- src/modules/chatMembers/chatMember.controller.ts
+++ src/modules/chatMembers/chatMember.controller.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
-import {createChatMemberSchema} from "./chatMember.model";
-import {createChatMember} from "./chatMember.service";
+import {createChatMemberSchema, deleteChatMemberSchema} from "./chatMember.model";
+import {createChatMember, deleteChatMember} from "./chatMember.service";
 
 export const handleCreateChatMember = async (req: Request, res: Response) => {
     try {
@@ -12,6 +12,30 @@ export const handleCreateChatMember = async (req: Request, res: Response) => {
 
         return res.status(201).json({
             message: 'Chat member created successfully',
+            data: chatMember
+        })
+    } catch (error) {
+        if (error instanceof Error && 'issues' in error) {
+            return res.status(400).json({
+                message: 'Validation error',
+                details: error.issues
+            })
+        }
+
+        return res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+export const handleDeleteChatMember = async (req: Request, res: Response) => {
+    try {
+        // validate request body using zod
+        const validateData = deleteChatMemberSchema.parse(req.body);
+
+        // call service to delete a chat member
+        const chatMember = await deleteChatMember(validateData);
+
+        return res.status(200).json({
+            message: 'Chat member deleted successfully',
             data: chatMember
         })
     } catch (error) {
