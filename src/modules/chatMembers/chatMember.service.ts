0ecdb9a7ea6e6b0f3e6c3013412b41a2218c0cf4
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-10 22:37:14 +0100

    refactor: update param type

--- src/modules/chatMembers/chatMember.service.ts
+++ src/modules/chatMembers/chatMember.service.ts
@@ -1,10 +1,10 @@
-import {CreateChatMemberSchema} from "./chatMember.model";
+import {CreateChatMemberSchema, DeleteChatMemberSchema} from "./chatMember.model";
 import prisma from "../../config/db";
 
 export const createChatMember = async (chatMember: CreateChatMemberSchema) => {
     return prisma.chat_member.create({data: chatMember})
 }
 
-export const deleteChatMember = async (chatMemberId: number) => {
-    return prisma.chat_member.delete({where: {id: chatMemberId}})
+export const deleteChatMember = async (chatMember: DeleteChatMemberSchema) => {
+    return prisma.chat_member.delete({where: {id: chatMember.id}})
 }
