df751c50c11338a6bb8fc7d62bb82998054c377d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-10 22:35:05 +0100

    build: delete chat member service

--- src/modules/chatMembers/chatMember.service.ts
+++ src/modules/chatMembers/chatMember.service.ts
@@ -4,3 +4,7 @@ import prisma from "../../config/db";
 export const createChatMember = async (chatMember: CreateChatMemberSchema) => {
     return prisma.chat_member.create({data: chatMember})
 }
+
+export const deleteChatMember = async (chatMemberId: number) => {
+    return prisma.chat_member.delete({where: {id: chatMemberId}})
+}
