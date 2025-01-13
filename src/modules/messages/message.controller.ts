99be0f5668d2e42f015fbab087e98cfeedaaad1a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-13 01:42:50 +0100

    build: get messages controller

--- src/modules/messages/message.controller.ts
+++ src/modules/messages/message.controller.ts
@@ -1,6 +1,6 @@
 import {Request, Response} from "express";
-import {createMessageSchema} from "./message.model";
-import {createMessage} from "./message.service";
+import {createMessageSchema, getMessagesSchema} from "./message.model";
+import {createMessage, getMessages} from "./message.service";
 
 export const handleCreateMessage = async (req: Request, res: Response) => {
     try {
@@ -13,6 +13,29 @@ export const handleCreateMessage = async (req: Request, res: Response) => {
         return res.status(200).json({
             message: 'Message created successfully',
             data: message
+        })
+    } catch (error) {
+        if (error instanceof Error && 'issues' in error) {
+            return res.status(400).json({
+                message: 'Validation error',
+                details: error.issues
+            })
+        }
+
+        return res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+export const handleGetMessages = async (req: Request, res: Response) => {
+    try {
+        // validate request body with zod
+        const validateData = getMessagesSchema.parse(req.body)
+
+        // call service to get all conversation messages
+        const messages = await getMessages(validateData)
+
+        return res.status(200).json({
+            messages
         })
     } catch (error) {
         if (error instanceof Error && 'issues' in error) {
