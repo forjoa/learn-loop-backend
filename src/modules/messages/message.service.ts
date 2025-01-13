1db693c4df01910733e934ff2104a3a93495ea84
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-13 01:41:17 +0100

    build: get messages service

--- src/modules/messages/message.service.ts
+++ src/modules/messages/message.service.ts
@@ -1,6 +1,30 @@
-import {CreateMessageSchema} from "./message.model";
+import {CreateMessageSchema, GetMessagesSchema} from "./message.model";
 import prisma from "../../config/db";
 
 export const createMessage = async (message: CreateMessageSchema) => {
     return prisma.message.create({data: message})
+}
+
+export const getMessages = async (chat: GetMessagesSchema) => {
+    return prisma.message.findMany({
+        where: {
+            chatId: chat.chatId,
+        },
+        select: {
+            id: true,
+            chatId: true,
+            senderId: true,
+            content: true,
+            createdAt: true,
+            sender: {
+                select: {
+                    id: true,
+                    name: true,
+                },
+            },
+        },
+        orderBy: {
+            createdAt: "asc",
+        },
+    });
 }
