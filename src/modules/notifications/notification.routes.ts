1c6758ba65db838fe4e084e08b046ff957689059
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-13 19:32:53 +0100

    feat: endpoint to get notifications

--- src/modules/notifications/notification.routes.ts
+++ src/modules/notifications/notification.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
-import { handleCreateNotification } from './notification.controller'
+import { handleCreateNotification, handleGetNotifications } from './notification.controller'
 
 const router = Router()
 
 // @ts-ignore
 router.post('/create', handleCreateNotification)
+// @ts-ignore
+router.get('/get', handleGetNotifications)
 
 export default router
