066a5f0d419b138fb6e7cb98ea97c22113f537e4
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-13 19:29:27 +0100

    build: get notifications service

--- src/modules/notifications/notification.service.ts
+++ src/modules/notifications/notification.service.ts
@@ -1,6 +1,14 @@
-import { CreateNotificationSchema } from './notification.model'
+import { CreateNotificationSchema, GetNotificationsSchema } from './notification.model'
 import prisma from '../../config/db'
 
 export const createNotification = async (notification: CreateNotificationSchema) => {
     return prisma.notification.create({data: notification})
+}
+
+export const getNofitications = async (notification: GetNotificationsSchema) => {
+    return prisma.notification.findMany({
+        where: {
+            userId: notification.userId
+        }
+    })
 }
