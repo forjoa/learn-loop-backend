0d124e7545fc37769cb00dcac551ce38ddcffb32
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-13 19:32:06 +0100

    build: get notifications controller

--- src/modules/notifications/notification.controller.ts
+++ src/modules/notifications/notification.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express'
-import { createNotificationSchema } from './notification.model'
-import { createNotification } from './notification.service'
+import { createNotificationSchema, getNotificationsSchema } from './notification.model'
+import { createNotification, getNofitications } from './notification.service'
 
 export const handleCreateNotification = async (req: Request, res: Response) => {
     try {
@@ -13,6 +13,29 @@ export const handleCreateNotification = async (req: Request, res: Response) => {
         return res.status(200).json({
             message: 'Notification created successfully',
             data: notification
+        })
+    } catch (error) {
+        if (error instanceof Error && 'issues' in error) {
+            return res.status(400).json({
+                message: 'Validation error',
+                details: error.issues
+            })
+        }
+
+        return res.status(500).json({message: 'Internal server error'})
+    }
+}
+
+export const handleGetNotifications = async (req: Request, res: Response) => {
+    try {
+        // validate request body with zod
+        const validateData = getNotificationsSchema.parse(req.body)
+
+        // call service to get a notification
+        const notifications = await getNofitications(validateData)
+
+        return res.status(200).json({
+            ...notifications
         })
     } catch (error) {
         if (error instanceof Error && 'issues' in error) {
