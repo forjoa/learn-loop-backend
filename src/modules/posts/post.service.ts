96d133ff6737a8070ce86d8a8c7d7d8a08e2b1b9
forjoa <joaquin_trujillo@icloud.com>
2025-05-11 16:27:51 +0200

    fix: add transactional insert

--- src/modules/posts/post.service.ts
+++ src/modules/posts/post.service.ts
@@ -2,62 +2,65 @@ import prisma from '../../config/db'
 import { CreatePost, GetSinglePost } from './post.model'
 
 export const createPost = async (postData: CreatePost) => {
-
-    console.log(postData)
-    const newPost = await prisma.post.create({
-        data: {
-            title: postData.title,
-            content: postData.content,
-            topicId: postData.topicId,
-            userId: postData.userId,
-        },
-    })
-
-
-    if (postData.fileUrl) {
-        const file = await prisma.file.create({
+    return prisma.$transaction(async (prisma) => {
+        const newPost = await prisma.post.create({
             data: {
-                filename: postData.filename,
-                fileType: postData.fileType,
-                url: postData.fileUrl,
-                postId: newPost.id,
+                title: postData.title,
+                content: postData.content,
+                topicId: postData.topicId,
+                userId: postData.userId,
             },
         })
 
-        console.log(file)
-    }
+        if (postData.fileUrl) {
+            try {
+                const file = await prisma.file.create({
+                    data: {
+                        filename: postData.filename,
+                        fileType: postData.fileType,
+                        url: postData.fileUrl,
+                        postId: newPost.id,
+                    },
+                })
 
-    const topic = await prisma.topic.findUnique({
-        where: {id: postData.topicId},
-        select: {title: true},
-    })
+                console.log(file)
+            } catch (e) {
+                console.log(e)
+            }
+        }
 
-    if (!topic) {
-        throw new Error(`Topic with id=${postData.topicId} not found`)
-    }
-
-    const approvedEnrollments = await prisma.enrollment.findMany({
-        where: {
-            topicId: postData.topicId,
-            status: 'APPROVED',
-        },
-        select: {userId: true},
-    })
-
-    const notifications = approvedEnrollments.map(({userId}) => ({
-        userId,
-        title: 'Nuevo post',
-        content: `Se ha creado nuevo post en ${topic.title}`,
-    }))
-
-    if (notifications.length > 0) {
-        await prisma.notification.createMany({
-            data: notifications,
-            skipDuplicates: true,
+        const topic = await prisma.topic.findUnique({
+            where: {id: postData.topicId},
+            select: {title: true},
         })
-    }
 
-    return newPost
+        if (!topic) {
+            throw new Error(`Topic with id=${postData.topicId} not found`)
+        }
+
+        const approvedEnrollments = await prisma.enrollment.findMany({
+            where: {
+                topicId: postData.topicId,
+                status: 'APPROVED',
+            },
+            select: {userId: true},
+        })
+
+        const notifications = approvedEnrollments.map(({userId}) => ({
+            userId,
+            title: 'Nuevo post',
+            content: `Se ha creado nuevo post en ${topic.title}`,
+        }))
+
+        if (notifications.length > 0) {
+            await prisma.notification.createMany({
+                data: notifications,
+                skipDuplicates: true,
+            })
+        }
+
+        return newPost
+    })
 }
 
 export const getSinglePost = async (postData: GetSinglePost) => {
